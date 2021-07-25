e1e8e878cf4e68bafa85b56065d382675e2c09d4
sajibsaha4647 <sksaha4648@gmail.com>
2021-07-26 01:27:41 +0600

    successfully created roles

--- database/seeders/Rolepermissionseeder.php
+++ database/seeders/Rolepermissionseeder.php
@@ -3,7 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 class Rolepermissionseeder extends Seeder
 {
     /**
@@ -13,6 +14,47 @@ class Rolepermissionseeder extends Seeder
      */
     public function run()
     {
-        //
+        $superadmin = Role::create(['name' => 'superadmin']);
+        $admin = Role::create(['name' => 'admin']);
+        $author = Role::create(['name' => 'author']);
+        $editor = Role::create(['name' => 'editor']);
+        $member = Role::create(['name' => 'member']);
+
+
+        $permissions =[
+
+            //deshboard
+            'deshboard.view',
+
+            //admin permission
+            'blog.create',
+            'blog.edit',
+            'blog.delete',
+            'blog.view',
+            'blog.approve',
+
+            //role permission
+            'role.create',
+            'role.edit',
+            'role.delete',
+            'role.view',
+            'role.approve',
+
+            //profile permission
+            'profile.create',
+            'profile.edit',
+            'profile.delete',
+            'profile.view',
+
+        ];
+
+        //assign permission
+        foreach($permissions as $permission ){
+
+              Permission::create(['name' => $permission]);
+            $superadmin->givePermissionTo($permission);
+
+        }
+
     }
 }
--- database/seeders/Userseeder.php
+++ database/seeders/Userseeder.php
@@ -2,7 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+use function PHPUnit\Framework\isNull;
 
 class Userseeder extends Seeder
 {
@@ -13,6 +17,17 @@ class Userseeder extends Seeder
      */
     public function run()
     {
-        //
+
+        $user = User::where('email','sajib@gmail.com')->first();
+
+        if(is_null($user)){
+            $user = new User();
+            $user->name = "sajib saha";
+            $user->email = "sajib@gmail.com";
+            $user->password = Hash::make('12345678');
+            $user->save();
+        }
+
+
     }
 }
